c7a888b429c822cb840a2e3e4b62175ad44ed2d9
Adam Bradbury <f.adambrad@gmail.com>
2018-02-23 16:45:35 -0700

    forgot to remove backslash on config file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'https://best-sitter.netlify.com/';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'https://best-sitter.netlify.com';
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/jwt-auth-demo';
 exports.PORT = process.env.PORT || 8080;
 (exports.JWT_SECRET = process.env.JWT_SECRET || 'foo'), (exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d');
